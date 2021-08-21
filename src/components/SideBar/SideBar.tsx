82f1aa1b94cf8d7281a96b7ebc3435f0d247a3ca
Yaroslav Kaniev <y.kaniev@gmail.com>
2021-08-21 19:31:18 +0300

    fix: update Tree component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -15,6 +15,7 @@ import {
   Tree,
   Intent,
   Icon,
+  TreeNodeInfo,
 } from "@blueprintjs/core";
 import { SideBarPanel1 } from "./SideBarPanel1";
 import { SideBarPanel2 } from "./SideBarPanel2";
@@ -23,12 +24,14 @@ import {
   ContextMenu2,
   Tooltip2,
 } from "@blueprintjs/popover2";
+import { TreeComponent } from "./TreeComponent";
+
+type NodePath = number[];
 
 export interface ICard {
   elevation: Elevation;
   interactive: boolean;
   animate: boolean;
-  nodes: any;
 }
 
 export class SideBar extends React.PureComponent {
@@ -36,7 +39,6 @@ export class SideBar extends React.PureComponent {
     elevation: 2,
     interactive: true,
     animate: true,
-    nodes: INITIAL_STATE,
   };
 
   public render() {
@@ -72,13 +74,7 @@ export class SideBar extends React.PureComponent {
         <div style={{ padding: "10px 15px 0px 15px" }}>
           <H5>Tree</H5>
         </div>
-        <Tree
-          contents={this.state.nodes}
-          // onNodeClick={handleNodeClick}
-          // onNodeCollapse={handleNodeCollapse}
-          // onNodeExpand={handleNodeExpand}
-          className={Classes.ELEVATION_0}
-        />
+        <TreeComponent />
       </Card>
     );
   }
@@ -93,92 +89,3 @@ export class SideBar extends React.PureComponent {
 const contentSizing = {
   popoverProps: { popoverClassName: Popover2Classes.POPOVER2_CONTENT_SIZING },
 };
-
-const INITIAL_STATE = [
-  {
-    id: 0,
-    hasCaret: true,
-    icon: "folder-close",
-    label: (
-      <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-        Folder 0
-      </ContextMenu2>
-    ),
-  },
-  {
-    id: 1,
-    icon: "folder-close",
-    isExpanded: true,
-    label: (
-      <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-        <Tooltip2 content="I'm a folder <3" placement="right">
-          Folder 1
-        </Tooltip2>
-      </ContextMenu2>
-    ),
-    childNodes: [
-      {
-        id: 2,
-        icon: "document",
-        label: "Item 0",
-        secondaryLabel: (
-          <Tooltip2 content="An eye!">
-            <Icon icon="eye-open" />
-          </Tooltip2>
-        ),
-      },
-      {
-        id: 3,
-        icon: (
-          <Icon
-            icon="tag"
-            intent={Intent.PRIMARY}
-            className={Classes.TREE_NODE_ICON}
-          />
-        ),
-        label:
-          "Organic meditation gluten-free, sriracha VHS drinking vinegar beard man.",
-      },
-      {
-        id: 4,
-        hasCaret: true,
-        icon: "folder-close",
-        label: (
-          <ContextMenu2 {...contentSizing} content={<div>Hello there!</div>}>
-            <Tooltip2 content="foo" placement="right">
-              Folder 2
-            </Tooltip2>
-          </ContextMenu2>
-        ),
-        childNodes: [
-          { id: 5, label: "No-Icon Item" },
-          { id: 6, icon: "tag", label: "Item 1" },
-          {
-            id: 7,
-            hasCaret: true,
-            icon: "folder-close",
-            label: (
-              <ContextMenu2
-                {...contentSizing}
-                content={<div>Hello there!</div>}
-              >
-                Folder 3
-              </ContextMenu2>
-            ),
-            childNodes: [
-              { id: 8, icon: "document", label: "Item 0" },
-              { id: 9, icon: "tag", label: "Item 1" },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 2,
-    hasCaret: true,
-    icon: "folder-close",
-    label: "Super secret files",
-    disabled: true,
-  },
-];
